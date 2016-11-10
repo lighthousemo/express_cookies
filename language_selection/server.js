8fd98f33a05af44e471ece9658cc88c89153fb9d
Monica Olinescu <monica.olinescu@gmail.com>
2016-11-10 09:44:38 -0800

    Remove old solution

--- language_selection/server.js
+++ language_selection/server.js
@@ -1,37 +1,18 @@
 const express = require("express")
-const Cookies = require('cookies')
-const KeyGrip = require("keygrip")
-const keys = new KeyGrip(['im a newer secret', 'i like turtle'], 'sha256')
 const app = express()
 const PORT = process.env.PORT || 8000; // default port 8080
-
-// Express middleware that parses cookies
-app.use((req, res, next) => {
-  req.cookies = new Cookies( req, res, { "keys": keys } )
-  next();
-})
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  console.log(req.cookies.get("language"));
-  const language = req.cookies.get("language")
-  if(language === "English") {
-    res.render("english")
-  } else if(language === "French") {
-    res.render("french")
-  } else {
-    res.render("choose_language")
-  }
+  res.render("choose_language")
 });
 
 app.get("/choose_english", (req, res) => {
-  req.cookies.set("language", "English");
   res.redirect("/");
 })
 
 app.get("/choose_french", (req, res) => {
-  req.cookies.set("language", "French");
   res.redirect("/");
 })
 
