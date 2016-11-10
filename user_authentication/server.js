50a483963c94e714af711d14705d4ecf5a623f4c
Monica Olinescu <monica.olinescu@gmail.com>
2016-11-10 15:14:47 -0800

    Simple login flow - without encryption

--- user_authentication/server.js
+++ user_authentication/server.js
@@ -1,45 +1,68 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const cookieSession = require('cookie-session')
+var cookieSession = require('cookie-session');
 const app = express()
 const PORT = process.env.PORT || 8000; // default port 8000
 
-// parse application/x-www-form-urlencoded form data into req.body
-app.use(bodyParser.urlencoded({ extended: false }))
-
 // set up the session store middleware
-app.set('trust proxy', 1) // trust first proxy ???
+app.set('trust proxy', 1) // trust first proxy
+
 app.use(cookieSession({
   name: 'session',
-  keys: ['supersecret', 'supersecret2']
+  keys: ['secretkey1', 'secretkey2']
 }))
+
+// parse application/x-www-form-urlencoded form data into req.body
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set("view engine", "ejs")
 
 const data = {
   users: [
-    {username: 'monica', password: 'testing'}
+    {username: 'monica', password: 'testing'},
+    {username: 'bob', password: 'testing'}
   ]
 }
 
 app.get("/", (req, res) => {
-  // req.session.favourite_fruit = 'apples';
-  res.render("login", {fruit: req.session.favourite_fruit})
+  res.render("login")
 });
 
-app.get("/favourite_fruit/:fruit", (req, res) => {
-  req.session.favourite_fruit = req.params.fruit;
-  res.redirect("/")
-})
+app.post("/login", (req, res) => {
+  console.log('data is ', data)
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // 1. Find the user with the given username
+  // Upgrade: Write this step using Array.find. One line of code!
+  var user;
+  data.users.forEach((userObj) => {
+    if(userObj.username === username) {
+      user = userObj;
+    }
+  });
+
+  if(user) {
+    // Check that the password from the form matches the password
+    // in the database.
+
+    if(password === user.password) {
+      // Store the username in a protected session.
+      req.session.username = user.username;
+      res.redirect("/treasure");
+    } else {
+      // Password is incorrect. Login failed.
+      res.redirect("/login");
+    }
+  } else {
+    // Username does not exist. Login failed.
+    // res.send("Wrong username or password.")
+    res.redirect("/login");
+  }
+});
 
 app.get("/login", (req, res) => {
-  res.render("login")
-})
-
-app.post("/login", (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
-  res.send("Implement me!")
+  res.render("login");
 })
 
 app.get("/signup", (req, res) => {
@@ -47,20 +70,31 @@ app.get("/signup", (req, res) => {
 })
 
 app.post("/signup", (req, res) => {
-  res.send("Implement me!")
+  const username = req.body.username;
+  const password = req.body.password;
+  const newUser = { username: username, password: password };
+  // Add the user to the list of registered users
+  data.users.push(newUser);
+  // TODO: log in the user automatically
+  res.redirect("/login");
 })
 
 app.get("/logout", (req, res) => {
-  res.redirect("/login")
-})
-
+  req.session.username = undefined;
+  res.redirect("/login");
+});
 
 app.get("/treasure", (req, res) => {
   // TODO: write code to only allow 
   // logged in users to see the page.
-  const current_user = {}
-  res.render("treasure", {current_user})
-})
+  console.log("!!!!!!!!", req.session);
+  const currentUsername = req.session.username;
+  if(currentUsername) {
+    res.render("treasure", { username: currentUsername }); 
+  } else {
+    res.redirect("/login");
+  } 
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
