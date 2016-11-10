aa8cb8d72fc1c8107851a303812be2c7051b7b34
Monica Olinescu <monica.olinescu@gmail.com>
2016-11-10 15:47:54 -0800

    Use bcrypt to store hashed passwords.

--- user_authentication/server.js
+++ user_authentication/server.js
@@ -1,15 +1,17 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-var cookieSession = require('cookie-session');
+const express = require('express')
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+
 const app = express()
 const PORT = process.env.PORT || 8000; // default port 8000
 
 // set up the session store middleware
 app.set('trust proxy', 1) // trust first proxy
-
-app.use(cookieSession({
-  name: 'session',
-  keys: ['secretkey1', 'secretkey2']
+app.use(session({
+  secret: 'keyboard ninja cat',
+  resave: false,
+  saveUninitialized: true
 }))
 
 // parse application/x-www-form-urlencoded form data into req.body
@@ -19,8 +21,7 @@ app.set("view engine", "ejs")
 
 const data = {
   users: [
-    {username: 'monica', password: 'testing'},
-    {username: 'bob', password: 'testing'}
+    {username: 'monica', password: "$2a$10$iKEbtLBDVa8xoH8YNoT0mu.IbL/A6utHPZbRb/s7PoZHRgZfZoBUq"}, // password is supersecret
   ]
 }
 
@@ -45,15 +46,16 @@ app.post("/login", (req, res) => {
   if(user) {
     // Check that the password from the form matches the password
     // in the database.
-
-    if(password === user.password) {
-      // Store the username in a protected session.
-      req.session.username = user.username;
-      res.redirect("/treasure");
-    } else {
-      // Password is incorrect. Login failed.
-      res.redirect("/login");
-    }
+    bcrypt.compare(password, user.password, function(err, passwordMatches){
+      if(passwordMatches) {
+        // Store the username in a protected session.
+        req.session.username = user.username;
+        res.redirect("/treasure");
+      } else {
+        // Password is incorrect. Login failed.
+        res.redirect("/login");
+      }
+    });
   } else {
     // Username does not exist. Login failed.
     // res.send("Wrong username or password.")
@@ -72,11 +74,16 @@ app.get("/signup", (req, res) => {
 app.post("/signup", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  const newUser = { username: username, password: password };
-  // Add the user to the list of registered users
-  data.users.push(newUser);
-  // TODO: log in the user automatically
-  res.redirect("/login");
+  const saltRounds = 10;
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    // Store hash in your password DB. 
+    const newUser = { username: username, password: hash };
+    // Add the user to the list of registered users
+    data.users.push(newUser);
+    // TODO: log in the user automatically
+    res.redirect("/login");
+  });
+
 })
 
 app.get("/logout", (req, res) => {
@@ -85,9 +92,6 @@ app.get("/logout", (req, res) => {
 });
 
 app.get("/treasure", (req, res) => {
-  // TODO: write code to only allow 
-  // logged in users to see the page.
-  console.log("!!!!!!!!", req.session);
   const currentUsername = req.session.username;
   if(currentUsername) {
     res.render("treasure", { username: currentUsername }); 
