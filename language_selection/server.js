5bf2cd6a9002384023cc8ab1d1e2d9f1f05737a4
Monica Olinescu <monica.olinescu@gmail.com>
2016-11-10 10:27:13 -0800

    Cookie example

--- language_selection/server.js
+++ language_selection/server.js
@@ -1,20 +1,34 @@
 const express = require("express")
+const cookieParser = require('cookie-parser')
 const app = express()
 const PORT = process.env.PORT || 8000; // default port 8080
 
 app.set("view engine", "ejs");
 
+// Set up cookie parser middleware
+app.use(cookieParser())
+
 app.get("/", (req, res) => {
-  res.render("choose_language")
+  console.log("Cookies are ", req.cookies)
+  if(req.cookies.language === "english") {
+    res.render("english")
+  } else if(req.cookies.language === "french") {
+    res.render("french")
+  } else {
+    res.render("choose_language")  
+  }
 });
 
 app.get("/choose_english", (req, res) => {
+  res.cookie('language', 'english');
   res.redirect("/");
 })
 
 app.get("/choose_french", (req, res) => {
+  res.cookie('language', 'french');
   res.redirect("/");
 })
+
 
 
 app.listen(PORT, () => {
