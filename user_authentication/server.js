8b5b9cad5297ad74fbd01fe31da2b2f732309782
Monica Olinescu <monica.olinescu@gmail.com>
2016-11-10 10:45:18 -0800

    Favourite fruit session example

--- user_authentication/server.js
+++ user_authentication/server.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cookieSession = require('cookie-session')
 const app = express()
 const PORT = process.env.PORT || 8000; // default port 8000
 
 // parse application/x-www-form-urlencoded form data into req.body
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// set up the session store middleware
+app.set('trust proxy', 1) // trust first proxy ???
+app.use(cookieSession({
+  name: 'session',
+  keys: ['supersecret', 'supersecret2']
+}))
 
 app.set("view engine", "ejs")
 
@@ -15,8 +23,14 @@ const data = {
 }
 
 app.get("/", (req, res) => {
-  res.render("login")
+  // req.session.favourite_fruit = 'apples';
+  res.render("login", {fruit: req.session.favourite_fruit})
 });
+
+app.get("/favourite_fruit/:fruit", (req, res) => {
+  req.session.favourite_fruit = req.params.fruit;
+  res.redirect("/")
+})
 
 app.get("/login", (req, res) => {
   res.render("login")
@@ -42,6 +56,8 @@ app.get("/logout", (req, res) => {
 
 
 app.get("/treasure", (req, res) => {
+  // TODO: write code to only allow 
+  // logged in users to see the page.
   const current_user = {}
   res.render("treasure", {current_user})
 })
